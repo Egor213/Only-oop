c5b7e0f3d646c568de892d29c641e3fd20096098
Egor213 <112801343+Egor213@users.noreply.github.com>
2023-12-03 23:22:04 +0300

    add_6lb

---
+++ lb6/Events/eventPattern.h
@@ -0,0 +1,13 @@
+#ifndef eventPattern_h
+#define eventPattern_h
+
+
+class GameEvent
+{
+public:
+    virtual void event() = 0;
+    virtual GameEvent* clone() = 0;
+    virtual ~GameEvent(){};
+};
+
+#endif
